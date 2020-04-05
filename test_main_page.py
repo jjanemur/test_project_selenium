a7e04ee9366c05599ddf04239ed86d14bda10e7c
jjanemur <jjanieva@gmail.com>
2020-04-05 21:26:42 +0300

    beautify call to login_page

--- test_main_page.py
+++ test_main_page.py
@@ -7,9 +7,5 @@ def test_guest_can_go_to_login_page(browser):#в аргументе фиксту
     page.open()
     page.should_be_login_link()
     page.go_to_login_page()
-
-def test_valid_login_page(browser):
-    link="http://selenium1py.pythonanywhere.com/accounts/login/"
-    page=LoginPage(browser,link)
-    page.open()
-    page.should_be_login_page()
+    login_page=LoginPage(browser,browser.current_url)
+    login_page.should_be_login_page()
