7062542438f62750ee88dd2cb81cb1747aa1ca7a
jjanemur <jjanieva@gmail.com>
2020-04-18 16:15:31 +0300

    moving some logic to BasePage, add paramatrized test

--- Pages/base_page.py
+++ Pages/base_page.py
@@ -1,5 +1,7 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+
+from .locators import BasePageLocators
 import math
 
 class BasePage():
@@ -8,8 +10,9 @@ class BasePage():
         self.url = url
         self.browser.implicitly_wait(timeout)
 
-    def open(self):
-        self.browser.get(self.url)
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
 
     def is_element_present(self, how, what):
         try:
@@ -17,6 +20,13 @@ class BasePage():
         except NoSuchElementException:
             return False
         return True
+
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,32 @@
 from .Pages.product_page import ProductPage
-import time
+import pytest
+from .Pages.login_page import LoginPage
 
-def test_guest_can_add_product_to_basket(browser):
-    link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+product_base_link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+promo_links=[f"{product_base_link}/?promo=offer{no}" for no in range(10)]
+@pytest.mark.parametrize("link",promo_links)
+def test_guest_can_add_product_to_basket(browser,link):#предаем параметризуемый аргумент
     page=ProductPage(browser,link)
     page.open()
     page.add_to_cart()
     page.solve_quiz_and_get_code()
     page.should_be_success_message()
     page.should_be_correct_price_of_cart()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
+
+
+
